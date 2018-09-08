4ed39037696ea1b03cb4b8bf3a7cbe8c47b275db
Jules Laplace <julescarbon@gmail.com>
2018-09-08 19:10:58 +0200

    sort toggle

--- src/memes/meme.index.js
+++ src/memes/meme.index.js
@@ -4,8 +4,21 @@ import Meme from './meme.component';
 import Create from '../create/create.component';
 
 class MemeIndex extends Component {
+  state = {
+    sort: 'recent',
+  }
+
   render() {
-    if (!this.props.ProxyFactory.events) return null;
+    if (!this.props.ProxyFactory.events) {
+      return (
+        <div>
+          <hr />
+          <div className='sortLinks'>
+            Loading memes...
+          </div>
+        </div>
+      );
+    }
 
     let { events } = this.props.ProxyFactory;
     let memes = events.map(meme => {
@@ -14,15 +27,34 @@ class MemeIndex extends Component {
       return <Meme key={address} address={address} />;
     });
 
+    if (this.state.sort === 'recent') {
+      memes.reverse();
+    }
+
     return (
       <div>
         <hr />
           <Create />
         <hr />
+        <div className='sortLinks'>
+          Sort memes by:
+          {this.sortLink('recent', 'Recent')}
+          {this.sortLink('price', 'Price')}
+        </div>
+        <hr />
         <div>
-          {memes.reverse()}
+          {memes}
         </div>
       </div>
+    );
+  }
+
+  sortLink(key, label) {
+    const className = (key === this.state.sort) ? 'active' : '';
+    return (
+      <u className={className} onClick={() => this.setState({ sort: key })}>
+        {'[' + label + ']'}
+      </u>
     );
   }
 }
