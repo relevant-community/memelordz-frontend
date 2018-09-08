24cbdfa4ecab82572bf9164ca3209e184f72062b
Jules Laplace <julescarbon@gmail.com>
2018-09-08 09:56:09 +0200

    header

---
+++ src/common/Header.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className={'header'}>
+      <h1>
+        Meme Lordz
+      </h1>
+      <p>
+        A fully decentralized curation market for memes! You can an ERC20 token
+        by uploading a meme and invest in memes by buying their tokens!
+      </p>
+      <p>
+        Built on Ethereum and IPFS
+      </p>
+    </div>
+  );
+}
