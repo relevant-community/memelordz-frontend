806bb070d16f0ed6b3d7bedb970eba4b354f36f7
Jules Laplace <julescarbon@gmail.com>
2018-09-11 10:01:54 +0200

    node_env=production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
     }),
     new UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin()
