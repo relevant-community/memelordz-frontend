a13c511e1b8f3f041da0b422cce1f0018a667922
Jules Laplace <julescarbon@gmail.com>
2018-09-13 00:28:14 +0200

    footer links

--- src/common/Footer.component.js
+++ src/common/Footer.component.js
@@ -9,6 +9,12 @@ function Footer() {
       <br /><br />
       <small>
         {'All trademarks and copyrights on this page are owned by their respective parties. Images uploaded are the responsibility of the Poster.'}
+        <br />
+        <a href="https://twitter.com/memelordzapp">Twitter</a>
+        {' • '}
+        <a href="https://t.me/meme_lordz">Telegram</a>
+        {' • '}
+        <a href="https://www.instagram.com/memelordzapp/">Insta</a>
       </small>
     </footer>
   );
